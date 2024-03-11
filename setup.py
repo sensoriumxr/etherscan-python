41ea8abdf13154ed9aa64c762c3411fa96498b13
maxim-zuev <maxim.zuev@sensoriumxr.com>
2024-03-11 17:45:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="etherscan-python",
     version="2.1.0",
     description="A minimal, yet complete, python API for etherscan.io.",
-    url="https://github.com/pcko1/etherscan-python",
+    url="https://github.com/maxim-zuev/etherscan-python",
     author="Panagiotis-Christos Kotsias",
     author_email="kotsias.pan@gmail.com",
     license="MIT",
